8798166f8d6ca7990437c8bce18885eb2a790225
RAHMASYIFATUNNABILAH <148332173+RAHMASYIFATUNNABILAH@users.noreply.github.com>
2024-01-19 13:17:41 +0700

    Add files via upload

---
+++ PBO_Pertemuan1/kerucut.py
@@ -0,0 +1,17 @@
+print("menghitung luas dan volume kerucut")
+
+#variabel
+r = 4
+t = 2
+la = 5
+ls = 8
+
+phi = 22/7
+
+#rumus
+Luas = la + ls
+Volume = 1/3 * (phi*r*r*t)
+
+#output
+print("Luas kerucut : ", Luas)
+print("Volume kerucut : ", Volume)
---
+++ PBO_Pertemuan1/kubus.py
@@ -0,0 +1,12 @@
+print("menghitung Luas dan Volume Kubus")
+
+#variabel
+r = 12
+
+#rumus
+Luas = 6*r*r
+Volume = r*r*r
+
+#output
+print("Luas kubus adalah  =", Luas)
+print("volume kubus adalah = ", Volume)
